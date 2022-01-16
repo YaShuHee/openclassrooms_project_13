685e011868991843483caae88cfe2eb271175356
Timothé Gélibert <tim.gelibert@protonmail.com>
2022-01-16 04:12:53 +0100

    Make 'oc_lettings_site' not an app

--- oc_lettings_site/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-
-# No more models here
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -21,7 +21,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'profiles.apps.ProfilesConfig',
     'lettings.apps.LettingsConfig',
-    'oc_lettings_site.apps.OCLettingsSiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
