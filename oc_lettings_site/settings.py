bba5a01041520cb94f4674bdd4308f6dcc3cc0de
Timothé Gélibert <tim.gelibert@protonmail.com>
2022-01-22 01:42:30 +0100

    Remove 'oc_lettings_site' for 'INSTALLED_APPS' and his 'apps.py' file + fix index template spacename

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -42,7 +42,6 @@ ALLOWED_HOSTS = ["oc-lettings-13.herokuapp.com", "localhost", "127.0.0.1"]
 INSTALLED_APPS = [
     'lettings.apps.LettingsConfig',
     'profiles.apps.ProfilesConfig',
-    'oc_lettings_site.apps.OCLettingsSiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
